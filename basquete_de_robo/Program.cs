efca2bfd9d8b5af29c879300e991cfdf3d62a785
ALUNO <ALUNO@LABFPC25>
2024-03-11 07:59:51 -0300

    Adicionar arquivos de projeto.

---
+++ basquete_de_robo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basquete_de_robo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+            d = int.Parse(Console.ReadLine());
+            if ( d <= 800)
+            {
+                Console.WriteLine("1");
+            }
+            else if (800 < d && 1400 >= d)
+            {
+                Console.WriteLine("2");
+            }
+            else if ( 1400 < d && 2000 >= d)
+            {
+                Console.WriteLine("3");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
